6a9e28e3eecc2fffc383c83c38f7c1f3af963dd6
Niscolinx <munisco12@gmail.com>
2020-04-08 10:23:01 +0100

    added async to the app.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import react, {Component} from 'react'
 import {Link, Route} from 'react-router-dom'
 import asyncComponent from './hoc/asyncComponent'
 
-import pizza from './containers/pizza'
-import users from './containers/users'
+import Pizza from './containers/pizza'
 
 
+const asyncUsers = asyncComponent(() => {
+    return import('./containers/users')
+})
 class App extends Component{
 
     render(){
-
+        <Route path='/' exact component = {Pizza}/>
+        <Route path='/Users' exact component = {asyncUsers}/>
 
         return(
             <Link to='/'>Pizza</Link>
