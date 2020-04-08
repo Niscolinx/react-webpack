9e5c881a3e6bbdea84fcd3c8626e50cbc8ede44a
Niscolinx <munisco12@gmail.com>
2020-04-08 09:50:01 +0100

    added the users

---
+++ src/containers/users.js
@@ -0,0 +1,14 @@
+import react, { Component } from 'react'
+
+
+class Users extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This are the Users</h1>
+            </div>
+        )
+    }
+}
+
+export default Users
