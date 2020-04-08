974bd801adf75c3f89a1ceeba07c10e422a85cd7
Niscolinx <munisco12@gmail.com>
2020-04-08 10:10:12 +0100

    added async to the app.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import react, {Component} from 'react'
+import {Link, route} from 'react-router-dom'
+import asyncComponent from './hoc/asyncComponent'
+
+import pizza from './containers/pizza'
+import users from './containers/users'
+
+
+class App extends Component{
+
+    render(){
+        return(
+
+        )
+    }
+}
