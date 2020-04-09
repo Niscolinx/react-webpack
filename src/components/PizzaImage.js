de2f5c070b62c53a10c45ba156e7b03d8be0e997
Niscolinx <munisco12@gmail.com>
2020-04-09 03:37:13 +0100

    updated the pizza image

--- src/components/PizzaImage.js
+++ src/components/PizzaImage.js
@@ -22,7 +22,6 @@ const pizza = (props) => {
         <img src = {pizzaImage} className = 'pizzaImg_main'/>
         <img src = {pizzaImage} className = 'pizzaImg_main'/>
         <img src = {pizzaImage} className = 'pizzaImg_main'/>
-        <img src = {pizzaImage} className = 'pizzaImg_main'/>
     </div>
 }
 
