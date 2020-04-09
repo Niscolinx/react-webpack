53a8f43250a7dca5413063df5a042e6a398bcf4b
Niscolinx <munisco12@gmail.com>
2020-04-09 22:42:22 +0100

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path')
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
     entry: './src/index.js',
-    file: 'bundle.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '',
