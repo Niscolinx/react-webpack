d246e9ced3de7f85f429f457f00fee206e8ada1a
Niscolinx <munisco12@gmail.com>
2020-04-12 21:30:14 +0100

    updated babel config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        chunkFiles: '[id].js',
+        chunkFilename: '[id].js',
         publicPath: '',
         filename: 'bundle.js'
 
