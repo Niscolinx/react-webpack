543d6c402d4dc4cf589a5f165c2592b11f62fe30
Niscolinx <munisco12@gmail.com>
2020-04-12 07:00:35 +0100

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
                 test: /\.js$/,
                 loader: '',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/
             }
         ]
     }
