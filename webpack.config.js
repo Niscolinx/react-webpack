ecd8aec5511dbfa132bfb69ecc26e0bb0b7624b9
Niscolinx <munisco12@gmail.com>
2020-04-12 22:30:55 +0100

    configured webpack

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                loader: '',
+                loader: 'babel-loader',
                 exclude: /node_modules/
             },
             {
