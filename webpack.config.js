71d6401ad5b3a9ef33834341449d9ede3eb4f848
Niscolinx <munisco12@gmail.com>
2020-04-13 08:25:40 +0100

    removed extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+//const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 module.exports = {
@@ -29,30 +29,30 @@ module.exports = {
                 }
             },
 
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                // If you are having trouble with urls not resolving add this setting.
-                                // See https://github.com/webpack-contrib/css-loader#url
-                                url: false,
-                                minimize: true,
-                                sourceMap: true
-                            }
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        }
-                    ]
-                })
-            },
+            // {
+            //     test: /\.scss$/,
+            //     use: ExtractTextPlugin.extract({
+            //         fallback: 'style-loader',
+            //         use: [
+            //             {
+            //                 loader: 'css-loader',
+            //                 options: {
+            //                     // If you are having trouble with urls not resolving add this setting.
+            //                     // See https://github.com/webpack-contrib/css-loader#url
+            //                     url: false,
+            //                     minimize: true,
+            //                     sourceMap: true
+            //                 }
+            //             },
+            //             {
+            //                 loader: 'sass-loader',
+            //                 options: {
+            //                     sourceMap: true
+            //                 }
+            //             }
+            //         ]
+            //     })
+            // },
 
             {
                 test: /\.css$/,
