92479298a994a9c615a95b7d483e535b120dd8e5
Niscolinx <munisco12@gmail.com>
2020-04-14 03:51:17 +0100

    update pizza image

--- src/components/PizzaImage.js
+++ src/components/PizzaImage.js
@@ -1,11 +1,11 @@
 import React from 'react'
-//import pizza from '../../public/assets/pizza.jpg'
+import pizza from '../../public/assets/pizza.jpg'
 
-const PizzaImage = (props) => {
+const PizzaImage = (props) => (
     <div className = 'pizzaImg'>
-        {/* <img src = {pizza} className = 'pizzaImg_main'/>       */}
+        <img src = {pizza} className = 'pizzaImg_main'/>      
     </div>
-}
+)
 
 
 export default PizzaImage;
