142de063d5cfb2e5350637ee86bf625f2dafb4a3
Niscolinx <munisco12@gmail.com>
2020-04-15 21:36:44 +0100

    updated webpack and installed rimraf

---
+++ webpack.prod.config.js
@@ -0,0 +1,108 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const isDevelopment = process.env.NODE_ENV === 'development'
+const htmlPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        chunkFilename: '[id].js',
+        publicPath: '',
+        filename: 'bundle.js'
+
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+            chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+        }),
+        new htmlPlugin({
+            template: __dirname + '/public/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        new webpack.ProvidePlugin({
+            "React": "react",
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    ],
+
+
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    babelrc: false,
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            },
+
+            {
+                test: /\.module\.s(a|c)ss$/,
+                loader: [
+                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            sourceMap: isDevelopment
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: isDevelopment
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            Ident: 'postcss',
+                            plugins: () => [
+                                autoprefixer({
+                                    browsers: [
+                                        "> 1%",
+                                        "last 2 versions"
+                                    ]
+                                })
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                exclude: /\.module.(s(a|c)ss)$/,
+                loader: [
+                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: isDevelopment
+                        }
+                    }
+                ]
+            },
+
+
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'url-loader?limit=20000&name=images/[name].[ext]'
+            }
+        ]
+    }
+}
