3713da0c41e49f859ed5c584c436288770776ebb
Niscolinx <munisco12@gmail.com>
2020-04-15 21:50:24 +0100

    updated package.json to run the production file on build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -30,7 +30,7 @@ module.exports = {
         new webpack.ProvidePlugin({
             "React": "react",
         }),
-        new webpack.optimize.UglifyJsPlugin()
+        new webpack.optimization.minimize()
     ],
 
 
